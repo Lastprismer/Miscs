5ce7816b212dec25772c618667e8ffb5566e9efe
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2022-06-06 23:55:03 +0800

    优化1ms
    通过递归函数可以提前return做了点小的优化
    删除了多余的sort（输入保证从小到大）
    合并了一个时间复杂度为n的循环
    他们缩到1ms，我真的不知道怎么做到的，开摆就对了

--- OJ/硬币.cpp
+++ OJ/硬币.cpp
@@ -7,31 +7,28 @@ using namespace std;
 typedef long long ll;
 int n, x;
 int a[202] = { 0 }, b[202] = { 0 };
-int dp[MAX] = {1}; // 01背包，dp[i][j]：从前i个硬币中选择，可凑成j元的数量，滚动数组优化为：直接凑成j元的数量
-int ans[MAX] = { 1 }; // 去掉a[i]币值之后，硬币能不能表示价格j
+int dp[MAX] = { 1 }; // 01背包，dp[i][j]：从前i个硬币中选择，可凑成j元的数量，滚动数组优化为：直接凑成j元的数量
+// ideal为欲凑成的数目，cur为检测的面值
+// 如果cur为必须的，那么含有cur、凑成ideal的数目，应当等于不含有cur、凑成ideal-cur的数目
+int kinds(int ideal, int cur)
+{
+	if (ideal < 0) return 0; // 提前return跳出递归，小优化
+	return dp[ideal] - kinds(ideal - cur, cur);
+}
 int main()
 {
 	cin >> n >> x;
 	for (int i = 1; i <= n; i++)
+        {                
 		cin >> a[i];
-	sort(a + 1, a + n + 1);
-	for (int i = 1; i <= n; i++)
-		for (int j = x; j >= a[i]; j--) // 背包的空间优化要从最大值开始遍历
+		for (int j = x; j >= a[i]; j--) // 从最大值开始遍历，简单的滚动数组优化01背包
 			//dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i]];
 			dp[j] += dp[j - a[i]];
+        }
 	int num = 0;
 	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 0; j <= x; j++)
-		{
-			if (j < a[i])
-				ans[j] = dp[j]; // 要的钱数小于面值，肯定不带自己
-			else
-				ans[j] = dp[j] - ans[j - a[i]];
-			//dp[j]表示可以凑成j面值的方法数（包括a[i]也可以不包括a[i]），ans[j-a[i]]表示不带a[i]可以凑成j-a[i] 的方法总数，两者相减就是不带上a[i]凑成j面值的方法数。
-		}
-
-		if (!ans[x])
+		if (!kinds(x, a[i]))
 			b[num++] = a[i];
 	}
 	cout << num << endl;
@@ -39,5 +36,4 @@ int main()
 	cout << b[0];
 	for (int i = 1; i < num; i++)
 		cout << ' ' << b[i];
-
 }
